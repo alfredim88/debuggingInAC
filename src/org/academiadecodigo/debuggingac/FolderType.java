60e339bf340f2dfa5fc0c5061c6592e320cdaede
Filipe <filipedpsilva@protonmail.com>
2018-05-29 17:05:53 +0000

    Mudei algumas pastas

--- src/org/academiadecodigo/debuggingac/FolderType.java
+++ src/org/academiadecodigo/debuggingac/FolderType.java
@@ -1,12 +1,13 @@
 package org.academiadecodigo.debuggingac;
 
 public enum FolderType {
-    CAR_CRASH("/resources/images/Folders-PNG-File.png"),
+
+    CAR_CRASH("/resources/images/game/folder_arabian-nights.png"),
     SNIPER_ELITE("/resources/images/Folders-PNG-File.png"),
-    GUESS_A_NUMBER("/resources/images/Folders-PNG-File.png"),
-    ARABIAN_NIGHT("/resources/images/Folders-PNG-File.png"),
-    HOTEL("/resources/images/Folders-PNG-File.png"),
-    MONEY_IN_THE_BANK("/resources/images/Folders-PNG-File.png"),
+    GUESS_A_NUMBER("/resources/images/game/folder_guess-a-number.png"),
+    ARABIAN_NIGHT("/resources/images/game/folder_arabian-nights.png"),
+    HOTEL("/resources/images/game/folder_hotel.png"),
+    MONEY_IN_THE_BANK("/resources/images/game/folder_arabian-nights.png"),
     ROCK_PAPER_SCISSORS("/resources/images/Folders-PNG-File.png"),
     HELLO_WORLD("/resources/images/Folders-PNG-File.png");
 
@@ -17,6 +18,9 @@ public enum FolderType {
     }
 
     public FolderType getRandomFolder(){
+
         return FolderType.values()[(int)(Math.random() * FolderType.values().length)];
+
     }
+
 }
