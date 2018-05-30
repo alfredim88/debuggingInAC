478c064460c32f46c9f5e1ed8a40e76ed909c9c4
Filipe <filipedpsilva@protonmail.com>
2018-05-30 23:08:14 +0000

    revert

--- src/org/academiadecodigo/debuggingac/Game.java
+++ src/org/academiadecodigo/debuggingac/Game.java
@@ -1,15 +1,20 @@
 package org.academiadecodigo.debuggingac;
 
+<<<<<<< HEAD
 import org.academiadecodigo.debuggingac.characters.Bug;
 import org.academiadecodigo.debuggingac.characters.CharactersFactory;
 import org.academiadecodigo.debuggingac.characters.Feature;
 import org.academiadecodigo.debuggingac.characters.Hittable;
+=======
+import org.academiadecodigo.debuggingac.characters.*;
+>>>>>>> parent of 42f88c0... Stuff
 import org.academiadecodigo.debuggingac.menu.Menu;
 import org.academiadecodigo.debuggingac.simplegraphics.pictures.Picture;
 
 
 public class Game implements Clickable {
 
+<<<<<<< HEAD
 
     private static final int FOLDERS_PER_ROW = 6;
     private static final int PADDING_FOLDERS = 200;
@@ -19,13 +24,27 @@ public class Game implements Clickable {
     private GameField gameField;
     private int mouseX;
     private int mouseY;
+=======
+    private static final int FOLDERS_PER_ROW = 6;
+    private static final int PADDING_FOLDERS = 200;
+    private static final int MARGIN_LEFT = 0;
+    private static final int MARGIN_TOP = 500;
+    private static final int TOTAL_CHARACTERS = 10;
+    private GameField gameField;
+    private volatile int mouseX;
+    private volatile int mouseY;
+>>>>>>> parent of 42f88c0... Stuff
     private boolean quit;
     private boolean finished;
     private int gameLevel = 1;
     private int lives = 3;
     private int score = 0;
     private int currentCharacter = 0;
+<<<<<<< HEAD
     private Hittable[] gameCharacters = new Hittable[TOTAL_CHARACTERS];
+=======
+    private Char[] gameCharacters = new Char[TOTAL_CHARACTERS];
+>>>>>>> parent of 42f88c0... Stuff
     private Picture[] folderPic = new Picture[FOLDERS_PER_ROW];
 
     public void init() throws InterruptedException {
@@ -35,6 +54,7 @@ public class Game implements Clickable {
         CharactersFactory factory = new CharactersFactory();
 
         for (int i = 0; i < TOTAL_CHARACTERS; i++) {
+<<<<<<< HEAD
             int random = (int) (Math.random() * 10);
 
             if (random > -1) {
@@ -43,13 +63,26 @@ public class Game implements Clickable {
             } else {
                 gameCharacters[i] = factory.createFeatures();
                 System.out.println("feature");
+=======
+
+            int random = (int) (Math.random() * 10);
+
+            if (random > 2) {
+                gameCharacters[i] = factory.createBugs();
+            } else {
+                gameCharacters[i] = factory.createFeatures();
+>>>>>>> parent of 42f88c0... Stuff
             }
         }
 
         //Grid for the folders
         for (int i = 0; i < FOLDERS_PER_ROW; i++) {
             String folderPath = FolderType.getRandomFolder().getFolderPic();
+<<<<<<< HEAD
             folderPic[i] = new Picture(MARGIN_LEFT + (PADDING_FOLDERS * i), MARGIN_TOP, "resources/images/folders/folder_arabian-nights.png");
+=======
+            folderPic[i] = new Picture(MARGIN_LEFT + (PADDING_FOLDERS * i), MARGIN_TOP, "" + folderPath);
+>>>>>>> parent of 42f88c0... Stuff
         }
 
         start();
@@ -60,6 +93,7 @@ public class Game implements Clickable {
 
         drawEverything();
 
+<<<<<<< HEAD
 
         while (!finished && currentCharacter < TOTAL_CHARACTERS) {
 
@@ -114,6 +148,48 @@ public class Game implements Clickable {
 
             Thread.sleep(5);
         }
+=======
+        while (!finished && currentCharacter < TOTAL_CHARACTERS) {
+
+            Char character = gameCharacters[currentCharacter];
+
+            while (!character.hasEnded() && !character.isSwattered()) {
+
+                character.move();
+
+                if (mouseX >= character.getX() && mouseX <= character.getOffsetX()
+                    && mouseY >= character.getY() && mouseY <= character.getOffsetY()) {
+
+                    character.hit();
+
+                    if (character instanceof Bug) {
+
+                        Bug bug = (Bug) character;
+                        score += bug.getPoints();
+                        gameField.updateScore(score);
+                        break;
+                    }
+
+                    lives--;
+                    gameField.updateLives(lives);
+                    break;
+                }
+
+                Thread.sleep(50);
+            }
+
+            if (lives == 0) {
+                finished = true;
+                gameOver();
+                return;
+            }
+            mouseX = 0;
+            mouseY = 0;
+            currentCharacter++;
+            Thread.sleep(1000);
+        }
+
+>>>>>>> parent of 42f88c0... Stuff
     }
 
     public void gameOver() throws InterruptedException {
--- src/org/academiadecodigo/debuggingac/characters/Char.java
+++ src/org/academiadecodigo/debuggingac/characters/Char.java
@@ -5,6 +5,7 @@ import org.academiadecodigo.debuggingac.simplegraphics.pictures.Picture;
 public abstract class Char {
 
     private boolean swattered;
+<<<<<<< HEAD
     private int x = randomFolder();
     private int y = 500;
     private Picture pic1;
@@ -21,10 +22,39 @@ public abstract class Char {
     }
 
 
+=======
+    private Picture pic1;
+    private Picture pic2;
+    private boolean goingUp = true;
+    private long topTimer;
+    private int x;
+    private int y = 500;
+
+    public Char(String pic1, String pic2){
+        this.x = Randomizer.randomFolder();
+        this.pic1 = new Picture(x,y,pic1);
+        this.pic2 = new Picture(x,y,pic2);
+    }
+
+    public void drawCharacter(){
+        pic1.draw();
+    }
+
+    public void hit() throws InterruptedException {
+        pic1.delete();
+        pic2.draw();
+        Thread.sleep(300);
+        pic2.delete();
+        swattered = true;
+        System.out.println("AUTCH!");
+    }
+
+>>>>>>> parent of 42f88c0... Stuff
     public boolean isSwattered() {
         return swattered;
     }
 
+<<<<<<< HEAD
 
     public void move(int speed) throws InterruptedException {
 
@@ -40,19 +70,41 @@ public abstract class Char {
 
             }
 
+=======
+    public void move(){
+
+        if (goingUp) {
+
+            pic1.translate(0,-10);
+            pic2.translate(0,-10);
+
+            if (reachTop()) {
+                goingUp = false;
+                topTimer = System.currentTimeMillis();
+            }
+>>>>>>> parent of 42f88c0... Stuff
             return;
         }
 
         if (System.currentTimeMillis() - topTimer > 2000) {
             pic1.translate(0, 10);
+<<<<<<< HEAD
 
             if (pic1.getY() == 500){
+=======
+            pic2.translate(0,10);
+
+            if (pic1.getY() >= 500){
+>>>>>>> parent of 42f88c0... Stuff
                 pic1.delete();
             }
         }
 
+<<<<<<< HEAD
 
 
+=======
+>>>>>>> parent of 42f88c0... Stuff
     }
 
     public boolean reachTop(){
@@ -61,6 +113,7 @@ public abstract class Char {
 
     }
 
+<<<<<<< HEAD
 
     public boolean hasEnded() {
 
@@ -93,4 +146,31 @@ public abstract class Char {
     public int getY() {
         return y;
     }
+=======
+    public boolean hasEnded() {
+
+        return !goingUp && pic1.getY() >= 500;
+    }
+
+    public void delete() {
+
+    }
+
+    public int getX(){
+        return pic1.getX();
+    }
+
+    public int getY(){
+        return pic1.getY();
+    }
+
+    public int getOffsetX() {
+        return pic1.getMaxX();
+    }
+
+    public int getOffsetY() {
+        return pic1.getMaxY();
+    }
+
+>>>>>>> parent of 42f88c0... Stuff
 }
