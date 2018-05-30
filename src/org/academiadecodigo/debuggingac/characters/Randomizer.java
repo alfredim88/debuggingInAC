0ba58ae827a917470e2264a1ba144234fc2bcfd4
defkorns <defkorns@gmail.com>
2018-05-30 14:19:18 +0000

    folder position

--- src/org/academiadecodigo/debuggingac/characters/Randomizer.java
+++ src/org/academiadecodigo/debuggingac/characters/Randomizer.java
@@ -3,7 +3,7 @@ package org.academiadecodigo.debuggingac.characters;
 public class Randomizer {
 
     public static int randomFolder() {
-        return 100 + (200 * (int) (Math.random() * 6));
+        return  200 * (int) (Math.random() * 6);
     }
 
 }
