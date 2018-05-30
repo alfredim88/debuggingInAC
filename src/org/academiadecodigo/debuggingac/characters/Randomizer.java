04337e227845c200d037b107300ffef932a6afd7
Filipe <filipedpsilva@protonmail.com>
2018-05-30 23:06:47 +0000

    ...

--- src/org/academiadecodigo/debuggingac/characters/Randomizer.java
+++
@@ -1,9 +0,0 @@
-package org.academiadecodigo.debuggingac.characters;
-
-public class Randomizer {
-
-    public static int randomFolder() {
-        return  200 * (int) (Math.random() * 6);
-    }
-
-}
