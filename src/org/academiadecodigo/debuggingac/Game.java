dd4a8be3276d35767ac9c01cb82703a9876e6ffc
Filipe <filipedpsilva@protonmail.com>
2018-05-30 23:15:27 +0000

    Revert "6"
    This reverts commit f5ea08c05db68e5c5ad22a7e7df52100a8454dc4.

---
+++ src/org/academiadecodigo/debuggingac/Game.java
@@ -0,0 +1,358 @@
+package org.academiadecodigo.debuggingac;
+
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of b3a5e0b... new stuff
+import org.academiadecodigo.debuggingac.characters.Bug;
+import org.academiadecodigo.debuggingac.characters.CharactersFactory;
+import org.academiadecodigo.debuggingac.characters.Feature;
+import org.academiadecodigo.debuggingac.characters.Hittable;
+<<<<<<< HEAD
+=======
+import org.academiadecodigo.debuggingac.characters.*;
+>>>>>>> parent of 42f88c0... Stuff
+=======
+import org.academiadecodigo.debuggingac.characters.*;
+>>>>>>> parent of 42f88c0... Stuff
+=======
+>>>>>>> parent of b3a5e0b... new stuff
+import org.academiadecodigo.debuggingac.menu.Menu;
+import org.academiadecodigo.debuggingac.simplegraphics.pictures.Picture;
+
+
+public class Game implements Clickable {
+
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+
+    private static final int FOLDERS_PER_ROW = 6;
+    private static final int PADDING_FOLDERS = 200;
+    private static final int MARGIN_LEFT = 70;
+    private static final int MARGIN_TOP = 500;
+    private static final int TOTAL_CHARACTERS = 5;
+    private GameField gameField;
+    private int mouseX;
+    private int mouseY;
+=======
+=======
+>>>>>>> parent of 42f88c0... Stuff
+=======
+
+>>>>>>> parent of b3a5e0b... new stuff
+    private static final int FOLDERS_PER_ROW = 6;
+    private static final int PADDING_FOLDERS = 200;
+    private static final int MARGIN_LEFT = 70;
+    private static final int MARGIN_TOP = 500;
+<<<<<<< HEAD
+<<<<<<< HEAD
+    private static final int TOTAL_CHARACTERS = 30;
+    private GameField gameField;
+    private volatile int mouseX;
+    private volatile int mouseY;
+>>>>>>> parent of 42f88c0... Stuff
+    private boolean quit;
+    private boolean finished;
+    private boolean levelFinished = false;
+=======
+    private static final int TOTAL_CHARACTERS = 10;
+=======
+    private static final int TOTAL_CHARACTERS = 5;
+>>>>>>> parent of b3a5e0b... new stuff
+    private GameField gameField;
+    private int mouseX;
+    private int mouseY;
+    private boolean quit;
+    private boolean finished;
+>>>>>>> parent of 42f88c0... Stuff
+    private int gameLevel = 1;
+    private int lives = 3;
+    private int score = 0;
+    private int currentCharacter = 0;
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    private Hittable[] gameCharacters = new Hittable[TOTAL_CHARACTERS];
+=======
+    private Char[] gameCharacters = new Char[TOTAL_CHARACTERS];
+>>>>>>> parent of 42f88c0... Stuff
+=======
+    private Char[] gameCharacters = new Char[TOTAL_CHARACTERS];
+>>>>>>> parent of 42f88c0... Stuff
+=======
+    private Hittable[] gameCharacters = new Hittable[TOTAL_CHARACTERS];
+>>>>>>> parent of b3a5e0b... new stuff
+    private Picture[] folderPic = new Picture[FOLDERS_PER_ROW];
+
+    public void init() throws InterruptedException {
+
+        gameField = new GameField();
+
+        CharactersFactory factory = new CharactersFactory();
+
+        for (int i = 0; i < TOTAL_CHARACTERS; i++) {
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+            int random = (int) (Math.random() * 10);
+
+            if (random > -1) {
+                gameCharacters[i] = factory.createBugs();
+                System.out.println("bug");
+            } else {
+                gameCharacters[i] = factory.createFeatures();
+                System.out.println("feature");
+=======
+=======
+>>>>>>> parent of 42f88c0... Stuff
+
+=======
+>>>>>>> parent of b3a5e0b... new stuff
+            int random = (int) (Math.random() * 10);
+
+            if (random > -1) {
+                gameCharacters[i] = factory.createBugs();
+                System.out.println("bug");
+            } else {
+                gameCharacters[i] = factory.createFeatures();
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> parent of 42f88c0... Stuff
+=======
+>>>>>>> parent of 42f88c0... Stuff
+=======
+                System.out.println("feature");
+>>>>>>> parent of b3a5e0b... new stuff
+            }
+        }
+
+        //Grid for the folders
+        for (int i = 0; i < FOLDERS_PER_ROW; i++) {
+            String folderPath = FolderType.getRandomFolder().getFolderPic();
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+            folderPic[i] = new Picture(MARGIN_LEFT + (PADDING_FOLDERS * i), MARGIN_TOP, "resources/images/folders/folder_arabian-nights.png");
+=======
+            folderPic[i] = new Picture(MARGIN_LEFT + (PADDING_FOLDERS * i), MARGIN_TOP, "" + folderPath);
+>>>>>>> parent of 42f88c0... Stuff
+=======
+            folderPic[i] = new Picture(MARGIN_LEFT + (PADDING_FOLDERS * i), MARGIN_TOP, "" + folderPath);
+>>>>>>> parent of 42f88c0... Stuff
+=======
+            folderPic[i] = new Picture(MARGIN_LEFT + (PADDING_FOLDERS * i), MARGIN_TOP, "resources/images/folders/folder_arabian-nights.png");
+>>>>>>> parent of b3a5e0b... new stuff
+        }
+
+        start();
+
+    }
+
+    public void start() throws InterruptedException {
+
+        drawEverything();
+
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+
+        while (!finished && currentCharacter < TOTAL_CHARACTERS) {
+
+            for (int i = 0; i < TOTAL_CHARACTERS; i++) {
+
+                showRandomCharacter();
+                currentCharacter++;
+                Thread.sleep(1000);
+            }
+
+        }
+
+    }
+
+    private void showRandomCharacter() throws InterruptedException {
+
+        Hittable character = gameCharacters[currentCharacter];
+
+        while (!character.hasEnded()) {
+
+            character.move();
+
+            if (mouseX >= character.getX() && mouseX <= character.getOffsetX()
+                    || mouseX + 50 >= character.getX() && mouseX <= character.getOffsetX()
+                    && mouseY >= character.getY() && mouseY <= character.getOffsetY()
+                    || mouseY + 50 >= character.getY() && mouseY <= character.getOffsetY()) {
+
+                if (character instanceof Bug) {
+
+                    Bug bug = (Bug) character;
+                    bug.hit();
+                    score += bug.getPoints();
+                    gameField.updateScore(score);
+                    return;
+                }
+
+
+                if (character instanceof Feature) {
+
+                    Feature feature = (Feature) character;
+                    feature.hit();
+                    lives--;
+                    gameField.updateScore(lives);
+                }
+
+            }
+
+            if (lives == 0) {
+                gameOver();
+                return;
+            }
+
+            Thread.sleep(5);
+        }
+=======
+        while (!finished && currentCharacter < TOTAL_CHARACTERS) {
+=======
+        while (!levelFinished && lives > 0) {
+>>>>>>> parent of 4724cfd... More stuff
+
+            while (!finished && currentCharacter < TOTAL_CHARACTERS) {
+
+                Char character = gameCharacters[currentCharacter];
+
+                while (!character.hasEnded() && !character.isSwattered()) {
+
+                    character.move();
+
+                    if (mouseX >= character.getX() && mouseX <= character.getOffsetX()
+                            && mouseY >= character.getY() && mouseY <= character.getOffsetY()) {
+
+                        character.hit();
+
+                        if (character instanceof Bug) {
+
+                            Bug bug = (Bug) character;
+                            score += bug.getPoints();
+                            gameField.updateScore(score);
+                            break;
+                        }
+
+                        lives--;
+                        gameField.updateLives(lives);
+                        break;
+                    }
+
+                    Thread.sleep(50);
+                }
+
+                if (lives == 0) {
+                    finished = true;
+                    gameOver();
+                    return;
+                }
+                mouseX = 0;
+                mouseY = 0;
+                currentCharacter++;
+                Thread.sleep(1000);
+                levelFinished = true;
+            }
+        }
+
+>>>>>>> parent of 42f88c0... Stuff
+=======
+=======
+
+>>>>>>> parent of b3a5e0b... new stuff
+        while (!finished && currentCharacter < TOTAL_CHARACTERS) {
+
+            for (int i = 0; i < TOTAL_CHARACTERS; i++) {
+
+                showRandomCharacter();
+                currentCharacter++;
+                Thread.sleep(1000);
+            }
+
+        }
+
+    }
+
+    private void showRandomCharacter() throws InterruptedException {
+
+        Hittable character = gameCharacters[currentCharacter];
+
+        while (!character.hasEnded()) {
+
+            character.move();
+
+            if (mouseX >= character.getX() && mouseX <= character.getOffsetX()
+                    || mouseX + 50 >= character.getX() && mouseX <= character.getOffsetX()
+                    && mouseY >= character.getY() && mouseY <= character.getOffsetY()
+                    || mouseY + 50 >= character.getY() && mouseY <= character.getOffsetY()) {
+
+                if (character instanceof Bug) {
+
+                    Bug bug = (Bug) character;
+                    bug.hit();
+                    score += bug.getPoints();
+                    gameField.updateScore(score);
+                    return;
+                }
+
+
+                if (character instanceof Feature) {
+
+                    Feature feature = (Feature) character;
+                    feature.hit();
+                    lives--;
+                    gameField.updateScore(lives);
+                }
+
+            }
+
+            if (lives == 0) {
+                gameOver();
+                return;
+            }
+
+<<<<<<< HEAD
+>>>>>>> parent of 42f88c0... Stuff
+=======
+            Thread.sleep(5);
+        }
+>>>>>>> parent of b3a5e0b... new stuff
+    }
+
+    public void gameOver() throws InterruptedException {
+        finished = true;
+        Picture gameOver = new Picture(0, 0, "resources/images/gameover.png");
+        gameOver.draw();
+        Thread.sleep(2000);
+    }
+
+    private void drawEverything() {
+
+        gameField.drawField();
+
+        for (int i = 0; i < TOTAL_CHARACTERS; i++) {
+            gameCharacters[i].drawCharacter();
+        }
+
+        for (int i = 0; i < FOLDERS_PER_ROW; i++) {
+            folderPic[i].draw();
+        }
+    }
+
+    @Override
+    public void setX(int x) {
+        mouseX = x;
+    }
+
+    @Override
+    public void setY(int y) {
+        mouseY = y;
+    }
+
+
+}
---
+++ src/org/academiadecodigo/debuggingac/menu/Menu.java
@@ -0,0 +1,112 @@
+
+package org.academiadecodigo.debuggingac.menu;
+
+import org.academiadecodigo.debuggingac.Clickable;
+
+
+public class Menu implements Clickable{
+
+    private MenuEvent menuEvent;
+    private volatile int mouseX;
+    private volatile int mouseY;
+    private Buttons play;
+    private Buttons tutorial;
+    private Buttons credits;
+    private Buttons back;
+    private boolean startSelection;
+    private boolean secondaryMenu;
+
+    public Menu() throws InterruptedException {
+
+        menuEvent = new MenuEvent();
+        //loading();
+        menuEvent.soundMenu();
+        play = ButtonFactory.getNewButton(ButtonType.PLAY);
+        tutorial = ButtonFactory.getNewButton(ButtonType.TUTORIAL);
+        credits = ButtonFactory.getNewButton(ButtonType.CREDITS);
+        back = ButtonFactory.getNewButton(ButtonType.BACK);
+        startSelection = false;
+        secondaryMenu = false;
+
+    }
+
+    public void selection() throws InterruptedException {
+
+        // menuEvent.init();
+        menuEvent.mainMenuLoop();
+
+        while (!startSelection) {
+
+            //back button
+            if (secondaryMenu) {
+                if ((mouseX >= back.getStartX() && mouseX <= back.getEndX()) &&
+                        (mouseY >= back.getStartY() && mouseY <= back.getEndY())) {
+                    menuEvent.removeCredits();
+                    menuEvent.removeTutorial();
+                    Thread.sleep(200);
+                    secondaryMenu = false;
+                    mainMenu();
+                }
+            }
+            /*
+             * If the player presses start
+             */
+            if ((mouseX >= play.getStartX() && mouseX <= play.getEndX()) &&
+                    (mouseY >= play.getStartY() && mouseY <= play.getEndY())) {
+                menuEvent.killSoundMenu();
+                startSelection = true;
+            }
+            /*
+             * If the the Credits submenu is selected
+             */
+            if ((mouseX >= tutorial.getStartX() && mouseX <= tutorial.getEndX()) &&
+                    (mouseY >= tutorial.getStartY() && mouseY <= tutorial.getEndY())) {
+                secondaryMenu = true;
+                tutorial();
+                menuEvent.removeCredits();
+            }
+            /*
+             * If the Tutorial submenu is selected
+             */
+            if ((mouseX >= credits.getStartX() && mouseX <= credits.getEndX()) &&
+                    (mouseY >= credits.getStartY() && mouseY <= credits.getEndY())) {
+                secondaryMenu = true;
+                credits();
+                menuEvent.removeTutorial();
+            }
+        }
+    }
+
+   /* private void loading() throws InterruptedException {
+
+        //menuEvent.init();
+        menuEvent.soundMenu();
+        mainMenu();
+
+    }*/
+    private void tutorial() {
+
+        menuEvent.tutorial();
+
+    }
+    private void credits() {
+
+        menuEvent.credits();
+
+    }
+
+    private void mainMenu() throws InterruptedException{
+
+        menuEvent.mainMenuLoop();
+
+    }
+
+    public void setX(int mouseX) {
+        this.mouseX = mouseX;
+    }
+
+    public void setY(int mouseY) {
+        this.mouseY = mouseY;
+    }
+
+}
