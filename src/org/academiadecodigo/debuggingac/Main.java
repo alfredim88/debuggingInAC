ec5746fd9e75e43715d6323a926eb0e04ab5a9da
defkorns <defkorns@gmail.com>
2018-05-31 15:18:05 +0000

    debug option
    set it true to remove menu

--- src/org/academiadecodigo/debuggingac/Main.java
+++ src/org/academiadecodigo/debuggingac/Main.java
@@ -3,16 +3,19 @@ package org.academiadecodigo.debuggingac;
 import org.academiadecodigo.debuggingac.menu.Menu;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
 
+    public static void main(String[] args) throws InterruptedException {
+        boolean debug = true;
         new Fullscreen();
         Swatter swatter = new Swatter();
 
         Menu menu = new Menu();
         swatter.setClickable(menu);
-
-        menu.selection();
-
+        if (debug) {
+            // menu.selection();
+        } else {
+            menu.selection();
+        }
         Game game = new Game();
         swatter.setClickable(game);
         game.init();
