f87545fff48fb19099f9df7ec1d91859f4d303ce
Alfredo Faria <alfredo.faria88@gmail.com>
2018-06-03 12:55:24 +0000

    trying to solve unix problems unsucessfully

--- src/org/academiadecodigo/debuggingac/Main.java
+++ src/org/academiadecodigo/debuggingac/Main.java
@@ -7,15 +7,14 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         boolean debug = false;
         new Fullscreen();
+
         Swatter swatter = new Swatter();
 
         Menu menu = new Menu();
         swatter.setClickable(menu);
-        if (debug) {
-            // menu.selection();
-        } else {
-            menu.selection();
-        }
+
+        menu.selection();
+
         Game game = new Game();
         swatter.setClickable(game);
         game.init();
