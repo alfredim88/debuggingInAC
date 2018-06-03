7fb497b71c3db286cfe6afa3a56357219050067f
DefKorns <defkorns@gmail.com>
2018-06-03 11:30:37 +0000

    clean

--- src/org/academiadecodigo/debuggingac/menu/MenuEvent.java
+++ src/org/academiadecodigo/debuggingac/menu/MenuEvent.java
@@ -22,12 +22,6 @@ public class MenuEvent {
     private static Picture loadingSeven;
     private static Picture loadingEight;
 
-    private static String OS = System.getProperty("os.name").toLowerCase();
-    public static boolean isUnix() {
-
-        return (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0 );
-
-    }
 
     public MenuEvent() {
 
